5f67ba3fe661725e870963cc236750778cb37784
Kevin Chan <kevinykchan@gmail.com>
2021-10-20 10:39:19 -0400

    Done with Loading Trip

--- src/App.js
+++ src/App.js
@@ -71,45 +71,38 @@ function App () {
             <div className='middle-center'>
               <h1 className='landing logo'>Taxi</h1>
               {
-                !isLoggedIn &&
-                <Link 
-                  id='signUp'
-                  className='btn btn-primary' 
-                  to='/sign-up'
-                >Sign up</Link>
+                !isLoggedIn && (
+                  <>
+                    <Link
+                      id='signUp'
+                      className='btn btn-primary'
+                      to='/sign-up'
+                    >Sign up</Link>
+                    <Link
+                      id='logIn'
+                      className='btn btn-primary'
+                      to='/log-in'
+                    >Log in</Link>
+                  </>
+                )
               }
               {
-                isRider() &&(
-                  <Link 
-                    id='logIn'
-                    className='btn btn-primary' 
+                isRider() && (
+                  <Link
+                    className='btn btn-primary'
                     to='/rider'
                   >Dashboard</Link>
                 )
               }
+              {
+                isDriver() && (
+                  <Link
+                    className='btn btn-primary'
+                    to='/driver'
+                  >Dashboard</Link>
+                )
+              }
             </div>
-          )} />
-          <Route path='/sign-up' render={() => (
-            isLoggedIn ? (
-              <Redirect to='/' />
-            ) : (
-              <SignUp />
-            )
-          )} />
-
-          {/* changed */}
-          <Route path='/log-in' render={() => (
-            isLoggedIn ? (
-              <Redirect to='/' />
-            ) : (
-              <LogIn logIn={logIn} />
-            )
-          )} />
-          <Route path='/driver' render={() => (
-            <Driver />
-          )} />
-          <Route path='/rider' render={() => (
-            <Rider />
           )} />
         </Switch>
       </Container>
